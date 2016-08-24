c9d24b00298b5c9baa8d8dfb3ec60e2b63933a98
leegh1992 <823896856@qq.com>
2016-08-24 22:53:09 +0800

    完成素数筛选法

--- src/com/leegh/array/Sushu.java
+++ src/com/leegh/array/Sushu.java
@@ -6,7 +6,7 @@ package com.leegh.array;
  */
 public class Sushu {
 
-    int solve1(int[] array, int n) {
+    public static int solve1(int[] array, int n) {
         int opt_Num = 0;
         for (int i = 2; i <= n; i++) {
             for (int j = 2; j <= i; j++) {
@@ -19,7 +19,7 @@ public class Sushu {
         return opt_Num;
     }
 
-    int solve2(int[] array, int n) {
+    public static int solve2(int[] array, int n) {
         int opt_Num = 0;
         for (int i = 2; i <= n; i++) {
             for (int j = 2; j * j < i; j++) {
@@ -32,11 +32,11 @@ public class Sushu {
         return opt_Num;
     }
 
-    int solve3(int[] array, int n) {
+    public static int solve3(int[] array, int n) {
         int opt_Num = 0;
         for (int i = 2; i <= n; i++) {
             if (array[i] == 0) {
-                for (int j = 2; j <= i; j++) {
+                for (int j = 2; i * j <= n; j++) {
                     array[i * j] = 1;
                     opt_Num++;
                 }
@@ -52,13 +52,29 @@ public class Sushu {
      * @param n
      * @return
      */
-    int solve4(int[] array, int n) {
+    public static int solve4(int[] array, int n) {
         int opt_Num = 0;
         int[] primeList = new int[n + 5];
+        for (int i = 2; i <= n; i++) {
+            if (array[i] == 0) {
+                primeList[++primeList[0]] = i;
+            }
+            for (int j = 0; j <= primeList[0] && i * primeList[j] <= n; j++) {
+                array[(i * primeList[j])] = 1;
+                opt_Num++;
+                if (i % primeList[j] == 0) {
+                    break;
+                }
+            }
+        }
         return opt_Num;
     }
 
     public static void main(String[] args) {
-
+        int[] arr1 = new int[105];
+        System.out.println(Sushu.solve1(arr1, 100));
+        System.out.println(Sushu.solve2(arr1, 100));
+        System.out.println(Sushu.solve3(arr1, 100));
+        System.out.println(Sushu.solve4(arr1, 100));
     }
 }
